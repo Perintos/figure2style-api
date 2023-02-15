b04b29ce2537b7c22c93748aba18c7837cac2b62
perintos <chateau.thomass@gmail.com>
2023-02-16 00:25:59 +0100

    modification type decrption de string vers string 1000

--- app/models/stylisticdevice.model.js
+++ app/models/stylisticdevice.model.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING
     },
     description: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING(1000)
     },
   }, {
     timestamps: false
